38f14d41ae4d51e5210daab781dcbcad06247e68
initial <veselypeta@gmail.com>
2021-04-28 17:23:29 +0100

    adds symbol table to mlir gen

--- lib/mlir-gen/mlirGen.cpp
+++ lib/mlir-gen/mlirGen.cpp
@@ -1,16 +1,19 @@
-
 #include "mlir-gen/mlirGen.h"
 #include "PCC/PCCOps.h"
 
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/Verifier.h"
-#include "mlir/Dialect/StandardOps/IR/Ops.h"
+
+#include "llvm/ADT/ScopedHashTable.h"
 
 #include <iostream>
+#include <set>
+#include <string>
 
 namespace {
 class MLIRGenImpl {
@@ -23,30 +26,18 @@ public:
     // save the filename - used for location tracking
     filename = compFile;
 
-    auto net = mlir::pcc::NetworkType::get(
-        builder.getContext(), mlir::pcc::NetworkType::Ordering::UNORDERED);
-
-    std::vector<mlir::Type> elems;
-    elems.push_back(net);
-    auto strctType = mlir::pcc::StructType::get(elems);
-
-    // auto set = mlir::pcc::SetType::get(net, 5);
-
-    // auto state = mlir::pcc::StateType::get(builder.getContext(), "I");
-
-    mlir::pcc::FooOp fooOp =
-    builder.create<mlir::pcc::FooOp>(loc(*ctx->const_decl()[0]->ID()->getSymbol()),
-    strctType); 
-    theModule.push_back(fooOp); 
-
-
-    //set the insertion point to the start of the module
+    // set the insertion point to the start of the module
     builder.setInsertionPointToStart(theModule.getBody());
 
+    // decalre a global scope
+    SymbolTableScopeT global_scope(symbolTable);
+
     // recursivelly call mlirGen
-    // if (mlir::failed(mlirGen(ctx))){
-    //   return nullptr;
-    // }
+    if (mlir::failed(mlirGen(ctx))) {
+      return nullptr;
+    }
+
+    // initially create global scope
 
     // Verify the module after we have finished constructing it, this will check
     // the structural properties of the IR and invoke any specific verifiers we
@@ -64,10 +55,36 @@ private:
   mlir::OpBuilder builder;
   std::string filename;
 
+  std::set<std::string> identStorage;
+
+  /// The symbol table maps a variable name to a value in the current scope.
+  /// Entering a function creates a new scope, and the function arguments are
+  /// added to the mapping. When the processing of a function is terminated, the
+  /// scope is destroyed and the mappings created in this scope are dropped.
+  llvm::ScopedHashTable<llvm::StringRef, mlir::Value> symbolTable;
+  using SymbolTableScopeT =
+      llvm::ScopedHashTableScope<llvm::StringRef, mlir::Value>;
+
   mlir::Location loc(const antlr4::Token &tok) const {
     return mlir::FileLineColLoc::get(builder.getContext(), "file",
                                      tok.getLine(),
                                      tok.getCharPositionInLine());
+  }
+
+  mlir::LogicalResult declare(std::string ident, mlir::Value val) {
+    // save the string to the class so that it won't be deleted, StrRef does not own the data
+    identStorage.insert(ident);
+    // get a reference to the stored data
+    // find will always succeed because we have inserted into the storage class
+    llvm::StringRef identRef = *identStorage.find(ident);
+
+    // shadowing not allowed!
+    if (symbolTable.count(identRef) > 0) {
+      assert(0 && "mlir value already delared in current scope!");
+      return mlir::failure();
+    }
+    symbolTable.insert(identRef, val);
+    return mlir::success();
   }
 
   mlir::LogicalResult mlirGen(ProtoCCParser::DocumentContext *ctx) {
@@ -86,10 +103,10 @@ private:
     mlir::IntegerType constType = builder.getI64Type();
     mlir::Location constDeclLocation = loc(*ctx->ID()->getSymbol());
 
-    // build a const decl op - TODO
-    builder.create<mlir::ConstantOp>(constDeclLocation, constType, builder.getI64IntegerAttr(constValue));
+    mlir::ConstantOp constOp = builder.create<mlir::ConstantOp>(
+        constDeclLocation, constType, builder.getI64IntegerAttr(constValue));
 
-    return mlir::success();
+    return declare(constId, constOp);
   }
 };
 } // namespace
