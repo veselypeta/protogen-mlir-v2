0d02df6670ad9b1e9de51725f8d59c2fe7a1d021
initial <veselypeta@gmail.com>
2021-05-13 09:35:51 +0100

    fix api change

--- lib/PCC/PCCOps.cpp
+++ lib/PCC/PCCOps.cpp
@@ -6,10 +6,9 @@ using namespace mlir::pcc;
 
 static void print(ProcessOp op, OpAsmPrinter &p) {
   auto procType = op.getType();
-  impl::printFunctionLikeOp(p, op, procType.getInputs(), /*isVariadic=*/false,
-                            procType.getResults());
+  mlir::function_like_impl::printFunctionLikeOp(
+      p, op, procType.getInputs(), /*isVariadic=*/false, procType.getResults());
 }
-
 
 #define GET_OP_CLASSES
 #include "PCC/PCC.cpp.inc"
