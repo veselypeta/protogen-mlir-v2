85c1d2f58aa8e53c2b32c0c82e5eacf9a82c77b1
veselypeta <veselypeta@gmail.com>
2021-10-21 15:25:26 +0100

    transient-states-pass

--- lib/PCC/PCCDialect.cpp
+++ lib/PCC/PCCDialect.cpp
@@ -1,7 +1,38 @@
 #include "PCC/PCCOps.h"
+#include "mlir/Transforms/InliningUtils.h"
 
 using namespace mlir;
 using namespace mlir::pcc;
+
+#include "PCC/PCCDialect.cpp.inc"
+
+//===----------------------------------------------------------------------===//
+// PCCInlinerInterface
+//===----------------------------------------------------------------------===//
+
+struct PCCInlinerInterface : public DialectInlinerInterface {
+  using DialectInlinerInterface::DialectInlinerInterface;
+
+  // All ops of PCC can legally be inlined
+  bool isLegalToInline(Operation *call, Operation *callable,
+                       bool wouldBeCloned) const override {
+    return true;
+  }
+  bool isLegalToInline(Region *dest, Region *src, bool wouldBeCloned,
+                       BlockAndValueMapping &valueMapping) const override {
+    return true;
+  }
+
+  bool isLegalToInline(Operation *op, Region *dest, bool wouldBeCloned, BlockAndValueMapping &valueMapping) const override{
+    return true;
+  }
+
+  void handleTerminator(Operation *op,
+                        ArrayRef<Value> valuesToReplace) const override {
+    // Do nothing for now;
+    // the only terminator is break; and it returns to values
+  }
+};
 
 void PCCDialect::initialize() {
   registerTypes();
@@ -10,6 +41,5 @@ void PCCDialect::initialize() {
 #define GET_OP_LIST
 #include "PCC/PCC.cpp.inc"
       >();
+  addInterfaces<PCCInlinerInterface>();
 }
-
-#include "PCC/PCCDialect.cpp.inc"
--- lib/PCC/Passes/TransientStatesPass.cpp
+++ lib/PCC/Passes/TransientStatesPass.cpp
@@ -3,8 +3,10 @@
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "mlir/Transforms/InliningUtils.h"
 #include <algorithm>
+#include <mlir/IR/BlockAndValueMapping.h>
 
 using namespace mlir;
+using namespace llvm;
 using namespace mlir::pcc;
 
 class ProcessOpPattern : public OpRewritePattern<ProcessOp> {
@@ -44,7 +46,7 @@ void TransientStatesPass::runOnOperation() {
       auto result =
           std::find_if(nestedOps.begin(), nestedOps.end(), is_transaction_op);
 
-      if (result != nestedOps.end()){
+      if (result != nestedOps.end()) {
         TransactionOp transOp = mlir::dyn_cast<TransactionOp>(*result);
         return rewrite_cb(transOp, rewriter);
       }
@@ -56,12 +58,59 @@ void TransientStatesPass::runOnOperation() {
         [](TransactionOp &op, PatternRewriter &rewriter) -> LogicalResult {
       ProcessOp parentProcess = op->getParentOfType<ProcessOp>();
       // insert a new set state operation before
-      rewriter.setInsertionPoint(op);
+      rewriter.setInsertionPointAfter(parentProcess);
       // TODO - figure out the action
-      StateType endState = StateType::get(rewriter.getContext(), "TransitionState");
-      rewriter.create<InlineConstOp>(op.getLoc(), endState);
-//      rewriter.create<StateUpdateOp>(op.getLoc(), );
 
+      // Workaround just append the name of the action to the current state name
+      StringRef procName = parentProcess.sym_name();
+      auto nestedOpsIt = op.getRegion().getOps<TransitionOp>();
+      std::for_each(
+          nestedOpsIt.begin(), nestedOpsIt.end(),
+          [&](TransitionOp transitionOp) {
+            // Create the new Op
+            std::string newProcName =
+                (procName + "_" + transitionOp.guard()).str();
+            Type machType = parentProcess.getType().getInput(0);
+            Type msgType = transitionOp.msgType();
+            FunctionType newFuncType =
+                rewriter.getFunctionType({machType, msgType}, llvm::None);
+            ProcessOp newProc = rewriter.create<ProcessOp>(
+                transitionOp.getLoc(), newProcName, newFuncType);
+            // create a constant op to facilitate the inlining
+            Block *entry = newProc.addEntryBlock();
+            rewriter.setInsertionPointToStart(entry);
+            auto inlineOp = rewriter.create<InlineConstOp>(
+                newProc.getLoc(), rewriter.getI64IntegerAttr(4));
+
+            // inline the regions
+            InlinerInterface inliner(rewriter.getContext());
+
+            // create a mapper for the arguments to the region
+            BlockAndValueMapping mapper;
+            mapper.map(parentProcess.getArgument(0),
+                       newProc.getArgument(0)); // map the mach ref
+            mapper.map(transitionOp.body().getArgument(0),
+                       newProc.getArgument(0)); // map the msg ref
+
+            LogicalResult wasInlined = inlineRegion(
+                /* inliner interface */ inliner,
+                /* src region */ &transitionOp.getRegion(),
+                /* inline point */ inlineOp,
+                /* mapper */ mapper,
+                /* results to replace */ {},
+                /* region result types */ {});
+
+            assert(succeeded(wasInlined) && "Failed to inline PCC function");
+
+            // remove the unnecessary inline const op
+            rewriter.eraseOp(inlineOp);
+
+          });
+
+      // TODO - Insert a set-state operation
+      //StringRef nextState = parentProcess.sym_name();
+
+      rewriter.eraseOp(op);
       return success();
     };
 
