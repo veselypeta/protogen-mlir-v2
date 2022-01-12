0987b8b9c1da15ba95df6ccbb9cb4300bc942824
veselypeta <veselypeta@gmail.com>
2022-01-12 21:04:01 +0000

    Adds tests for how to use symbol table

--- lib/FSM/Passes/StallingOptimizationPass.cpp
+++ lib/FSM/Passes/StallingOptimizationPass.cpp
@@ -14,7 +14,7 @@ using namespace mlir::fsm;
 namespace {
 
 // We need a custom rewriter to be able to construct it
-class ProtoGenRewriter : public PatternRewriter{
+class ProtoGenRewriter : public PatternRewriter {
   explicit ProtoGenRewriter(MLIRContext *ctx) : PatternRewriter{ctx}{
 
     // TODO - overwrite necessary methods here
@@ -30,11 +30,51 @@ public:
 
 // Implement the runOnOperation function
 void StallingOptimizationPass::runOnOperation() {
-  // grab the module
+  // grab the module/cache/directory
   ModuleOp theModule = OperationPass<ModuleOp>::getOperation();
+  MachineOp theCache = theModule.lookupSymbol<MachineOp>("cache");
+  MachineOp theDirectory = theModule.lookupSymbol<MachineOp>("directory");
 
-  // then walk each operation
+  /// Useful Lambdas ///
+  auto is_transient_state = [](TransitionOp &op){
+    // TODO - Implement how to detect a transient state
+    return true;
+  };
 
+  auto get_stable_start_state = [](TransitionOp &op){
+    // TODO - Implement Properly
+    return op.sym_name();
+  };
+
+  auto get_possible_racing_transactions = [&](llvm::StringRef startState){
+    auto stableStateRef = theCache.lookupSymbol<StateOp>(startState);
+
+  };
+
+
+  auto result = theModule.walk([&](TransitionOp transitionOp){
+    // Skip non-transient states
+    if(!is_transient_state(transitionOp))
+      return WalkResult::advance();
+
+    // Now we need to find the stable start state
+    llvm::StringRef stableStartState = get_stable_start_state(transitionOp);
+
+    // find out which message we have sent to the directory
+
+    // find the messages that can arrive in the stable start state
+
+
+
+
+
+
+
+    return WalkResult::advance();
+  });
+
+  if(result.wasInterrupted())
+    return signalPassFailure();
 }
 
 
@@ -46,7 +86,7 @@ namespace mlir {
 namespace fsm {
 
 std::unique_ptr<OperationPass<ModuleOp>> createStallingOptimizationPass() {
-  return nullptr;
+  return std::make_unique<StallingOptimizationPass>();
 }
 
 } // namespace fsm
--- test/FSM_Dialect/OpConstructionTests.cpp
+++ test/FSM_Dialect/OpConstructionTests.cpp
@@ -50,7 +50,8 @@ TEST(OpConstructionTests, PrintMachineOp) {
   machineOp.print(stream);
 
   auto expectedStr = "fsm.machine @cache() {\n"
-                     "  %some_count = fsm.variable \"some_count\" {initValue = 21 : i64} : i64\n"
+                     "  %some_count = fsm.variable \"some_count\" {initValue = "
+                     "21 : i64} : i64\n"
                      "}";
 
   ASSERT_STREQ(str.c_str(), expectedStr);
@@ -89,8 +90,8 @@ TEST(OpConstructionTests, StateOp) {
   Block *sEntry = stateOp.addEntryBlock();
   helper.builder.setInsertionPointToStart(sEntry);
   // add the constant
-  helper.builder.create<ConstantOp>(
-      helper.builder.getUnknownLoc(), helper.builder.getI64IntegerAttr(21));
+  helper.builder.create<ConstantOp>(helper.builder.getUnknownLoc(),
+                                    helper.builder.getI64IntegerAttr(21));
 
   // print operation to string
   std::string str;
@@ -177,7 +178,7 @@ TEST(OpConstructionTests, ParseTransitionOp) {
 
   auto result = parseSourceString(opText, &helper.ctx);
 
-  result->walk([](TransitionOp op){
+  result->walk([](TransitionOp op) {
     ASSERT_TRUE(op.nextState().hasValue());
     ASSERT_TRUE(op.nextState().getValue().getLeafReference() == "M");
     ASSERT_TRUE(op.sym_name() == "load");
@@ -185,4 +186,83 @@ TEST(OpConstructionTests, ParseTransitionOp) {
 
   auto logRes = result->verify();
   ASSERT_TRUE(succeeded(logRes));
+}
+
+TEST(OpConstructionTests, SymbolTableTest) {
+  OpHelper helper;
+  Location unknLoc = helper.builder.getUnknownLoc();
+
+  FunctionType machType =
+      helper.builder.getFunctionType(llvm::None, llvm::None);
+  MachineOp machOp =
+      helper.builder.create<MachineOp>(unknLoc, "cache", machType);
+  Block *entry = machOp.addEntryBlock();
+  helper.builder.setInsertionPointToStart(entry);
+
+  helper.builder.create<VariableOp>(unknLoc, helper.builder.getI64Type(),
+                                    helper.builder.getI64IntegerAttr(21),
+                                    "some_value");
+
+  // create a state
+  StateOp sState = helper.builder.create<StateOp>(unknLoc, "S");
+  Block *sEntry = sState.addEntryBlock();
+  helper.builder.setInsertionPointToStart(sEntry);
+
+  // create a transition
+  TransitionOp sLoadTran = helper.builder.create<TransitionOp>(
+      unknLoc, helper.builder.getStringAttr("load"),
+      helper.builder.getSymbolRefAttr("M"));
+  Block *sLoadTransBlock = sLoadTran.addEntryBlock();
+  helper.builder.setInsertionPointToStart(sLoadTransBlock);
+
+  // add a dummy op
+  helper.builder.create<ConstantOp>(unknLoc,
+                                    helper.builder.getI64IntegerAttr(21));
+
+  // create a new state
+  helper.builder.setInsertionPointAfter(sState);
+  StateOp mState = helper.builder.create<StateOp>(unknLoc, "M");
+  Block *mEntry = mState.addEntryBlock();
+  helper.builder.setInsertionPointToStart(mEntry);
+
+  // add a transition to M state
+  TransitionOp mLoadTran = helper.builder.create<TransitionOp>(
+      unknLoc, helper.builder.getStringAttr("load"),
+      helper.builder.getSymbolRefAttr("S"));
+  Block *mLoadTranEntry = mLoadTran.addEntryBlock();
+  helper.builder.setInsertionPointToStart(mLoadTranEntry);
+  // add a dummy op
+  helper.builder.create<ConstantOp>(unknLoc,
+                                    helper.builder.getI64IntegerAttr(33));
+
+  /// Now we want to check how we can use Symbol Table to get the information
+  /// we need about transactions
+
+  // 1 - get the cache
+  auto cacheOp = helper.module.lookupSymbol<MachineOp>("cache");
+  ASSERT_NE(cacheOp, nullptr);
+
+  // 2 - get the S state within the cache
+  auto sStateLookUp = cacheOp.lookupSymbol<StateOp>("S");
+  ASSERT_NE(sStateLookUp, nullptr);
+
+  // 3 - find a transition
+  auto sLoadTransLookUp = sStateLookUp.lookupSymbol<TransitionOp>("load");
+  ASSERT_NE(sLoadTransLookUp, nullptr);
+  ASSERT_TRUE(sLoadTransLookUp.nextState().getValue().getLeafReference() ==
+              "M");
+
+  // 4 - try and lookup the state linked from
+  auto nextStateLookUp = cacheOp.lookupSymbol<StateOp>(
+      sLoadTransLookUp.nextStateAttr().getLeafReference());
+  ASSERT_NE(nextStateLookUp, nullptr);
+
+  // 5 - go directly to a transition from cache Op
+  auto symbolRef = helper.builder.getSymbolRefAttr(
+      "S", {helper.builder.getSymbolRefAttr("load")});
+
+  auto dirTrans = cacheOp.lookupSymbol<TransitionOp>(symbolRef);
+  ASSERT_NE(dirTrans, nullptr);
+
+  // 6 - works with colons?
 }
